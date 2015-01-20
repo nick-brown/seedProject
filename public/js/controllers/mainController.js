67ece3c904d80f4764f9251078163ab8bf09f0d7
Nick Brown <nick@nickbrown.me>
2015-01-19 23:38:22 -0800

    basic user form with validation added

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,10 +1,9 @@
 module.exports = function($scope) {
     'use strict';
-    $scope.todos = [
-        { action: "Get groceries", complete: false },
-        { action: "Call plumber", complete: false },
-        { action: "Buy running shoes", complete: true },
-        { action: "Buy flowers", complete: false },
-        { action: "Call family", complete: false }
-    ];
+    $scope.addUser = function(userDetails) {
+        $scope.message = userDetails.name + " (" + userDetails.email +
+        ") (" + userDetails.agreed + ")";
+    };
+
+    $scope.message = 'Ready';
 };
