67fc54607fd5a10e01a3a16df1e326fed8afe849
Nick Brown <nick@nickbrown.me>
2015-01-19 22:14:21 -0800

    basic todo app setup

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,3 +1,10 @@
 module.exports = function($scope) {
     'use strict';
+    $scope.todos = [
+        { action: "Get groceries", complete: false },
+        { action: "Call plumber", complete: false },
+        { action: "Buy running shoes", complete: true },
+        { action: "Buy flowers", complete: false },
+        { action: "Call family", complete: false }
+    ];
 };
