60a1a18b6e31d798523db3062287f94edd89db91
Nick Brown <nick@nickbrown.me>
2015-03-11 16:44:59 -0700

    directives do not update via service, will only update if inherited from scope

--- public/js/ngmodules/authors/authors.js
+++ public/js/ngmodules/authors/authors.js
@@ -1,18 +1,37 @@
-module.exports = angular.module('authors', [])
-    .service('authorService', function($q, $http, author, $rootScope) {
-        'use strict';
+module.exports = (function() {
+    'use strict';
+    return angular.module('authors', [])
+        .service('authorService', function($q, $http, author) {
 
-        this.getNext = function() {
-            return $http.get('/api/authors')
-                .then(function(res) {
-                    author.data = res.data;
-                    console.log(res.data);
+            this.getNext = function() {
+                return $http.get('/api/authors')
+                    .then(function(res) {
+                        author.data = res.data;
+                        //console.log(res.data);
 
-                    return res.data;
-                });
-        };
-    })
-    
-    .service('author', function() {
+                        return res.data;
+                    });
+            };
+        })
+        
+        .service('author', function() {
+            this.data = {};
+        })
 
-    });
+        .directive('top', function() {
+            return {
+                restrict: 'E',
+                template: '<div>{{author.name}}</div>'
+            };
+        })
+        
+        .directive('bottom', function() {
+            return {
+                restrict: 'E',
+                template: '<div ng-repeat="book in books">{{book.title}}</div>',
+                controller: function($scope, author) {
+                    $scope.books = author.data.books;
+                }
+            };
+        });
+}());
