350dbb82cd6282b459379f2acf3a3444fc2adb68
Nick Brown <nick@nickbrown.me>
2015-04-09 11:43:37 -0600

    removed old comments

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,15 +3,6 @@ module.exports = function($scope, products, items) {
 
     $scope.message = 'Hello';
 
-    //$scope.products = products.fetch();
-    //window.prods = $scope.products;
-    //$scope.$watch('products', function(nu, old) {
-    //    window.nu = nu;
-    //    window.old = old;
-    //    console.log('nu', nu);
-    //    console.log('old', old);
-    //});
-
     $scope.increment = function(num) {
         return ++num;
     };
@@ -19,9 +10,4 @@ module.exports = function($scope, products, items) {
     items.fetchItems().then(function(data) {
         $scope.items = data;
     });
-
-    //products.fetch().then(function(res) {
-    //    console.log(res.data);
-    //    $scope.products = res.data;
-    //});
 };
