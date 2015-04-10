05f577d7363b1197586190326dc2f96dce2d995d
Nick Brown <nick@nickbrown.me>
2015-04-09 21:35:17 -0600

    moved server files to a more central location

--- server.js
+++ server.js
@@ -1,52 +1,62 @@
 /*globals __dirname, process*/
+
 // MODULES
 //==============================================================================
+
 var express    = require('express')
-  , app        = express()
-  , bodyParser = require('body-parser')
-  , port       = process.env.PORT || 8080;
+,   app        = express()
+,   bodyParser = require('body-parser')
+,   port       = process.env.PORT || 8080
+,   SERVER     = __dirname + '/server'
+,   DIST       = __dirname + '/dist';
 
 
-// APP CONFIG
+// SERVER CONFIG
 //==============================================================================
-app.use(express.static(__dirname + '/dist'));
+
+app.use(express.static(DIST));
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.set('port', port);
 
+
 // MIDDLEWARE
 //==============================================================================
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('views', __dirname + '/dist/');
+app.set('views', DIST);
 app.set('view engine', 'jade');
 
 
 // DB SETUP
 //==============================================================================
-var db = require('./config/db');
+
+var db = require(SERVER + '/config/db');
 db.connect();
 
 // MODELS
 var models = {
-    Product: require('./app/models/product'),
+    Product: require(SERVER + '/models/product'),
 };
 
 
 // ROUTES
 //==============================================================================
+
 app
     .get('/', function(req, res) {
         'use strict';
-        res.sendFile(__dirname + '/dist/index.html');
+        res.sendFile(DIST);
     });
 
 
-require('./app/routes')(express.Router(), app, models);
+require(SERVER + '/routes')(express.Router(), app, models);
 
 
 // KICK THE TIRES AND LIGHT THE FIRES
 //==============================================================================
+
 module.exports = app;
